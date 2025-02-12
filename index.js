0115647f40af970aafed4e101430a37eb692e177
bjgonzalesgUNT <t022700120@unitru.edu.pe>
2025-02-12 11:19:49 -0500

    Formatear el código en index.js para mejorar la legibilidad

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { chromium } = require("playwright");
 const fs = require("fs");
 
-
 const main = async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({
+    headless: false,
+  });
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  const url = "https://www.laborum.pe/job/BCP---Otras-posiciones/Practicante-Pre-Profesional/67a6138fb3e5207fb22e0d56";
+  const url =
+    "https://www.laborum.pe/job/BCP---Otras-posiciones/Practicante-Pre-Profesional/67a6138fb3e5207fb22e0d56";
 
   //  *Go to web
   await page.goto(url);
@@ -28,49 +30,48 @@ const main = async () => {
   const data = [];
 
   for (const item of items) {
-
     // *Title
     const titleElement = await item.$("h6");
-    const title = titleElement && await titleElement.textContent();
+    const title = titleElement && (await titleElement.textContent());
 
     // *Enterprise
     const enterpriseElement = await item.$("h6 + p");
-    const enterprise = enterpriseElement && await enterpriseElement.textContent();
+    const enterprise =
+      enterpriseElement && (await enterpriseElement.textContent());
 
     // *Location
     const [locationElement] = await item.$$("ul > li");
-    const location = locationElement && await locationElement.textContent();
+    const location = locationElement && (await locationElement.textContent());
 
     // *PublishedAt
     const publishedAtElement = await item.$("ul + p");
-    const publishedAt = publishedAtElement && await publishedAtElement.textContent();
+    const publishedAt =
+      publishedAtElement && (await publishedAtElement.textContent());
 
     // *Description
     await item.click();
     await page.waitForSelector("h4 + div");
 
     const descriptionElement = await page.$("h4 + div");
-    const description = descriptionElement && await descriptionElement.textContent();
+    const description =
+      descriptionElement && (await descriptionElement.textContent());
 
     data.push({
       title,
       enterprise,
       location,
       publishedAt,
-      description
+      description,
     });
-
   }
 
   // *Save file
   saveFile(data);
 
-
   await page.close();
   await context.close();
   await browser.close();
-}
-
+};
 
 const saveFile = (items) => {
   try {
@@ -78,8 +79,8 @@ const saveFile = (items) => {
   } catch (error) {
     throw Error(error);
   }
-}
+};
 
 (async () => {
   await main();
-})()
+})();
